67bda176f3f9be5d7da6ae928a30d1b0c7e6253f
Simone Anna Kuster <simoneannakuster@stud-237-56.eduroam.unibe.ch>
2015-12-15 18:21:12 +0100

    again123213

---
+++ scripts/Lernen.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	$(function () {
+		$('[data-toggle="tooltip"]').tooltip();
+	});
+	
+	$('#Albanisch-button').click(function () {
+		
+		$('#Albanisch').removeClass("hidden");
+		
+	});
+	
+	$('#Deutsch-button').click(function () {
+		
+		$('#Deutsch').removeClass("hidden"); 
+		
+	});
+	
+});
