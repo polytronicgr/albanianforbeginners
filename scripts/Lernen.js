38de842e858bcf05ab4c9a69a34d2282b1a820ea
Simone Anna Kuster <simoneannakuster@Simone-Anna-Kusters-MacBook-Pro.local>
2015-12-16 08:00:28 +0100

    gitcommittoday1

--- scripts/Lernen.js
+++ scripts/Lernen.js
@@ -1,18 +1,26 @@
 $(document).ready(function() {
-	$(function () {
-		$('[data-toggle="tooltip"]').tooltip();
-	});
-	
-	$('#Albanisch-button').click(function () {
-		
-		$('#Albanisch').removeClass("hidden");
-		
-	});
-	
-	$('#Deutsch-button').click(function () {
-		
-		$('#Deutsch').removeClass("hidden"); 
-		
-	});
-	
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+     
+    $('#Albanisch-button').click(function () {
+    if($('#Albanisch').hasClass('hidden')) {
+        $('#Albanisch').removeClass('hidden')
+    }
+    else {
+    $('#Albanisch').addClass('hidden');
+    }
+         
+    });
+     
+    $('#Deutsch-button').click(function () {
+    if($('#Deutsch').hasClass('hidden')) {
+        $('#Deutsch').removeClass('hidden')
+    }
+    else {
+    $('#Deutsch').addClass('hidden');
+    }
+         
+    });
+     
 });
